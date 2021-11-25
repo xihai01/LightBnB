4bc299c846e6cd685558b7489c925325edb25d3f
Xihai Luo <xihai.luo01@gmail.com>
2021-11-25 03:02:34 +0000

    add more seeds to db

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  email_address VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
@@ -17,36 +17,36 @@ CREATE TABLE properties (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   cover_photo_url VARCHAR(255) NOT NULL,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
-  num_of_bedrooms INTEGER NOT NULL DEFAULT 0,
-  num_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  province VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  province VARCHAR(255) NOT NULL,
-  post_code VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL DEFAULT TRUE,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  post_code VARCHAR(255) NOT NULL
 );
 
 -- reservations schema
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
 );
 
 -- property_reviews schema
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY,
-  message TEXT,
-  rating SMALLINT NOT NULL DEFAULT 0,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,5 +1,5 @@
 -- add seeds to users table (schema)
-INSERT INTO users (name, email_address, password)
+INSERT INTO users (name, email, password)
 -- owners
 VALUES ('Xihai Luo', 'xihai.luo01@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
@@ -12,96 +12,96 @@ VALUES ('Xihai Luo', 'xihai.luo01@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4E
 
 -- add seeds to properties table
 INSERT INTO properties (
+  owner_id,
   title,
   description,
-  cover_photo_url,
   thumbnail_photo_url,
+  cover_photo_url,
   country,
   street,
   city,
   province,
   post_code,
-  active,
-  owner_id
+  active
 )
 VALUES (
+  1,
   'Speed Lamp',
   'description',
-  'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',
   'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',
+  'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',
   'Canada',
   '30 Thunder Grove',
   'Toronto',
   'Ontario',
   'M1V 4A3',
-  'true',
-  1
+  'true'
 ),
 (
+  1,
   'Blank Corner',
   'description',
-  'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
   'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
+  'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
   'Canada',
   '1211 AppleTree Road',
   'Calgary',
   'Manitoba',
   '123453',
-  'true',
-  1
+  'true'
 ),
 (
+  3,
   'Poop Station',
   'description',
-  'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
   'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350',
+  'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
   'Sweden',
   '33 Poopy Drive',
   'Stockholm',
   'Meatball',
   '125z31',
-  'true',
-  3
+  'true'
 ),
 (
+  2,
   'Trail Mix',
   'description',
-  'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg',
   'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&h=350',
+  'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg',
   'Canada',
   '43 Snowden Drive',
   'Vancover',
   'British Colombia',
   'abcde',
-  'true',
-  2
+  'true'
 ),
 (
+  4,
   'Game Fill',
   'description',
-  'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg',
   'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350',
+  'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg',
   'Turkey',
   '0011 Iskandar Kebap Road',
   'Trapezon',
   'Kunefe',
   '332df32',
-  'true',
-  4
+  'true'
 );
 
 -- add seeds to reservations table
-INSERT INTO reservations (start_date, end_date, guest_id, property_id)
-VALUES ('2018-09-11', '2018-09-26', 6, 1),
-('2019-01-04', '2019-02-01', 5, 2),
-('2021-10-01', '2021-10-14', 7, 1),
-('2016-07-17', '2016-08-01', 6, 4),
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 6),
+('2019-01-04', '2019-02-01', 2, 5),
+('2021-10-01', '2021-10-14', 1, 7),
+('2016-07-17', '2016-08-01', 4, 6),
 ('2018-03-10', '2018-03-25', 5, 5);
 
 -- add seeds to property_reviews table
-INSERT INTO property_reviews (message, rating, guest_id, reservation_id, property_id)
-VALUES ('messages', 7, 6, 1, 1),
-('messages', 10, 5, 2, 2),
-('messages', 5, 7, 3, 1),
-('messages', 2, 6, 4, 4),
-('messages', 8, 5, 5, 5);
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (6, 1, 1, 7, 'messages'),
+(5, 2, 2, 10, 'messages'),
+(7, 1, 3, 5, 'messages'),
+(6, 4, 4, 2, 'messages'),
+(5, 5, 5, 8, 'messages');
